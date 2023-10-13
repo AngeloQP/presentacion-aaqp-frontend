fa15afe80d88dedfa75346b524d9e99692a3b487
Angelo QP <angquerevalu@gmail.com>
2023-10-13 13:50:41 -0500

    Primer Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Constantes } from '@data/constantes/constantes';
+import { SkeletonComponent } from '@layout/skeleton/skeleton.component';
+import { HomeModule } from '@modules/home/home.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+   path: Constantes.EMPTY_STRING,
+   component: SkeletonComponent,
+   children: [
+    {
+     path: Constantes.RUTA_HOME,
+     loadChildren: () => import('@modules/home/home.module').then((m): typeof HomeModule => m.HomeModule),
+    },
+    { path: '**', redirectTo: Constantes.EMPTY_STRING, pathMatch: 'full' },
+   ],
+  },
+  { path: '**', redirectTo: Constantes.EMPTY_STRING, pathMatch: 'full' },
+ ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/data/constantes/constantes.ts
@@ -0,0 +1,4 @@
+export class Constantes{
+    public static EMPTY_STRING = '';
+    public static RUTA_HOME = '';
+}
